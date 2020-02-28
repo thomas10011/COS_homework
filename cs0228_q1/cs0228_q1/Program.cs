67a6b51988fb934c173421728dc163e46c037304
thomas10011 <tanhuiri@126.com>
2020-02-28 18:00:39 +0800

    m

---
+++ cs0228_q1/cs0228_q1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace cs0228_q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            Program p = new Program(num);
+
+        }
+        
+        Program(string n)
+        {
+            int num = Convert.ToInt32(n);
+            find(num);
+        }
+
+        private void find(int num)//寻找因子
+        {
+            for(int i = 2; Convert.ToDouble(i) <= Math.Sqrt(Convert.ToDouble(num)); i++)
+            {
+                if (num % i == 0)
+                {
+                    if (isPrimeNuim(i))
+                    {
+                        Console.WriteLine(Convert.ToString(i));
+                        
+                    }
+                    if (isPrimeNuim(num / i))
+                    {
+                        Console.WriteLine(Convert.ToString(num / i));
+                    }
+                }
+            }
+        }
+
+
+
+        private bool isPrimeNuim(int num)//判断是否是素数
+        {
+            for(int i = 2; Convert.ToDouble(i) <= Math.Sqrt(Convert.ToDouble(num)); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ cs0228_q3/cs0228_q3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace cs0228_q3
+{
+    class Program
+    {
+        private int[] a = new int[101];
+        static void Main(string[] args)
+        {
+            Program run = new Program();
+        }
+        Program()
+        {
+            for(int i = 0; i < 101; i++)
+            {
+                a[i] = 1;
+            }
+            a[0] = 0;
+            a[1] = 0;
+            for(int i = 2; i < 101; i++)
+            {
+                if (a[i] == 1)
+                {
+                    for(int j = i; j<=100; j += i)
+                    {
+                        a[j] = 0;
+                    }
+                    Console.WriteLine(Convert.ToString(i));
+                }
+
+
+            }
+        }
+
+
+
+    }
+}
