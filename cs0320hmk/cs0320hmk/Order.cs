3ba9f103a2dff16bedd0bcf830391828390ca851
thomas10011 <tanhuiri@126.com>
2020-03-20 16:19:16 +0800

    更新Oder类

--- cs0320hmk/cs0320hmk/Order.cs
+++ cs0320hmk/cs0320hmk/Order.cs
@@ -7,22 +7,49 @@ namespace cs0320hmk
 
     class Order
     {
-        Order()//constructor
+        public Order(int ID, int ItemNumber)//constructor  ItemNumber表示订单项的数量
         {
-
+            orderTime = DateTime.Now; //初始化当前时间
+            orderNumber = ID; //初始化订单号
+            createOrder(ItemNumber);
         }
+        private int orderNumber;//订单编号
+        private DateTime orderTime;//订单创建时间
 
 
-        OrderItem[] items;
 
-        public bool insertItem()
+        List<OrderItem> items;
+
+        private bool insertItem(string name, double price, int num)
         {
-
-
+            OrderItem newItem = new OrderItem(name, price, num);
+            items.Add(newItem);
 
             return true;
         }
+        private void createOrder(int ItemNum)
+        {
+            string name;
+            double price;
+            int num;
 
+            for (int i = 0; i < ItemNum; i++)
+            {
+                
+                Console.WriteLine("请输入商品名称，以回车结束");
+                name = Console.ReadLine();
+
+                Console.WriteLine("请输入商品名称，以回车结束");
+                price = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("请输入商品名称，以回车结束");
+                num = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+
+
+        }
 
 
     }
