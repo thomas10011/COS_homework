58141ea9b6a0d3e0439bde674e36b5c114bf14e5
thomas10011 <tanhuiri@126.com>
2020-03-20 15:57:15 +0800

    新增3月20号作业

---
+++ cs0320hmk/cs0320hmk/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cs0320hmk
+{
+
+    class Order
+    {
+        Order()//constructor
+        {
+
+        }
+
+
+        OrderItem[] items;
+
+        public bool insertItem()
+        {
+
+
+
+            return true;
+        }
+
+
+
+    }
+}
---
+++ cs0320hmk/cs0320hmk/OrderItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cs0320hmk
+{
+  
+
+    class OrderItem
+    {
+        private  string name;//商品名称
+
+        private double price;//商品单价
+        private double totalPrice;
+
+        private int number;//商品数量
+
+        public string getName()
+        {
+            return name;
+        }
+        public double getPice()
+        {
+            return price;
+        }
+        public double getTotalPrice()
+        {
+            return number * price;
+        }
+        public int getNum()
+        {
+            return number;
+        }
+
+        OrderItem(string name, double price, int num)
+        {
+            this.name = name;
+            this.price = price;
+            number = num;
+        }
+
+    }
+}
